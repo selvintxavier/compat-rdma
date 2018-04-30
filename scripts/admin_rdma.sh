a3f3ca1b2dcc856e2e3470dd3a5bae937cac8db5
Vladimir Sokolovsky <vlad@mellanox.com>
2018-04-30 14:03:02 -0500

    scripts: Updated list of linux files to include in the package
    Signed-off-by: Vladimir Sokolovsky <vlad@mellanox.com>

--- scripts/admin_rdma.sh
+++ scripts/admin_rdma.sh
@@ -229,6 +229,7 @@ mkdir -p $COMPAT include/{linux,net}
 echo "Copying $GIT_COMPAT_TREE/ files..."
 cp $GIT_COMPAT_TREE/compat/*.[ch] $COMPAT/
 cp $GIT_COMPAT_TREE/compat/Makefile $COMPAT/
+cp $GIT_COMPAT_TREE/compat/Kconfig $COMPAT/
 cp $GIT_COMPAT_TREE/autogen.sh $COMPAT/
 cp $GIT_COMPAT_TREE/configure.ac $COMPAT/
 cp -a $GIT_COMPAT_TREE/build $COMPAT/
